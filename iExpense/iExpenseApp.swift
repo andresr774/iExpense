8de2473984ab396d8711fc20896e936cb3c672d5
Andres <andresr774@gmail.com>
2022-03-02 11:02:42 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Andres camilo Raigoza misas on 2/03/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
